bf0ad404903adec2add8f12cd1981f715734e7d8
mahmudul <mahmudul.info.bd@gmail.com>
2021-10-18 00:31:31 +0200

    Gridview simple from abhi

--- app/src/main/java/com/example/baseadapterexample/MainActivity.java
+++ app/src/main/java/com/example/baseadapterexample/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.GridView;
 public class MainActivity extends Activity {
 
     GridView simpleGrid;
+    //sending all the images to the custom adapter
     int animals[] = {R.drawable.animal13, R.drawable.animal14, R.drawable.animal15, R.drawable.animal16, R.drawable.animal17, R.drawable.animal18, R.drawable.animal15, R.drawable.animal16, R.drawable.animal17
             ,R.drawable.animal14, R.drawable.animal15, R.drawable.animal16, R.drawable.animal17, R.drawable.animal18, R.drawable.animal15, R.drawable.animal16, R.drawable.animal17
             ,R.drawable.animal14, R.drawable.animal15, R.drawable.animal16, R.drawable.animal17, R.drawable.animal18, R.drawable.animal15, R.drawable.animal16, R.drawable.animal17
@@ -17,8 +18,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         simpleGrid = (GridView) findViewById(R.id.simpleGridView);
+        //creating the object of customAdapter and sending array and context as parameters
         CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), animals);
+       //adapter is set on gridview
         simpleGrid.setAdapter(customAdapter);
     }
 }
